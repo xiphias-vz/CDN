7271749127c1991c4011e351ba7e7ac04cfe73bb
Dražen Vincek <dvincek8@gmail.com>
2025-09-03 10:50:11 +0200

    Update giftcard.js

--- bear-and-merlin/js/giftcard.js
+++ bear-and-merlin/js/giftcard.js
@@ -8,42 +8,4 @@
                 link.href = link.href.replace('kiel.bear-and-merlin.com', 'bear-and-merlin.com');
             });
         }
-         // Find the problematic content container
-            const giftCardContent = document.getElementById('GiftCardFullCMSContent');
-            
-            if (giftCardContent) {
-                // Extract just the body content from the malformed HTML
-                const originalHTML = giftCardContent.innerHTML;
-                
-                // Try to extract just the body content
-                let cleanHTML = originalHTML;
-                
-                // Remove the nested html, head and body tags but keep their content
-                cleanHTML = cleanHTML.replace(/<html[^>]*>/gi, '')
-                                    .replace(/<\/html>/gi, '')
-                                    .replace(/<head[^>]*>[\s\S]*?<\/head>/gi, '')
-                                    .replace(/<body[^>]*>/gi, '')
-                                    .replace(/<\/body>/gi, '');
-                
-                // Update the content with the cleaned HTML
-                giftCardContent.innerHTML = cleanHTML;
-                
-                // Re-initialize any scripts that might be in the content
-                const scripts = giftCardContent.getElementsByTagName('script');
-                for (let i = 0; i < scripts.length; i++) {
-                    if (scripts[i].src) {
-                        // For external scripts
-                        const newScript = document.createElement('script');
-                        newScript.src = scripts[i].src;
-                        document.head.appendChild(newScript);
-                    } else {
-                        // For inline scripts
-                        try {
-                            eval(scripts[i].innerHTML);
-                        } catch (e) {
-                            console.error('Error executing script:', e);
-                        }
-                    }
-                }
-            }
     });
